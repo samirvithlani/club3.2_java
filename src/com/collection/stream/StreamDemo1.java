cbc56a3a9f311c5217f2abc11bdffa375fa04e81
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-12 14:30:08 +0530

    stream

---
+++ src/com/collection/stream/StreamDemo1.java
@@ -0,0 +1,44 @@
+package com.collection.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamDemo1 {
+
+	
+	public static void main(String[] args) {
+		
+		//Stream interface..
+		List<String> names = Arrays.asList("Amit","shyam","sundar","daya","jetha");
+		//names.stream().forEach(System.out::println);
+		//names.stream().filter(x->x.startsWith("s")).forEach(System.out::println);
+		
+//		List<String> filtnames = new ArrayList<String>();
+//		for(String s:names) {
+//			if(s.startsWith("s")) {
+//				filtnames.add(s);
+//			}
+//		}
+//		
+//		for(String s :filtnames) {
+//			System.out.println(s);
+//		}
+		
+//		List<String> filtNames = names.stream().filter(x->x.startsWith("s")).collect(Collectors.toList());
+//		filtNames.stream().forEach(System.out::println);
+		
+		
+		//names.stream().map(String::toUpperCase).forEach(System.out::println);
+		List<String> upperNames = names.stream().map(String::toUpperCase).collect(Collectors.toList());
+		upperNames.stream().forEach(System.out::println);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
