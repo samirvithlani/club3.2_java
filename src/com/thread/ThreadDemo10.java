6b1f8342dfa312db362e596ab6d0733325496595
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-24 15:37:22 +0530

    wait notify

--- src/com/thread/ThreadDemo10.java
+++ src/com/thread/ThreadDemo10.java
@@ -2,81 +2,65 @@ package com.thread;
 
 class Calling {
 
-	// 0 1
-	public synchronized void doCall(int flag) {
+    public synchronized void doCall(int flag) {
+        System.out.println(Thread.currentThread().getName() + " is making a call...");
 
-		System.out.println("Current call is running....."+Thread.currentThread().getName() +"'s call");
-		if (flag == 1) {
-			System.out.println("New call is on waiting....");
-			System.out.println("Current running call is puttin on hold....");
-			System.out.println(Thread.currentThread().getName() + " is putting to wait.....");
-			try {
-				wait();
-				System.out.println("New call is received..");
-				System.out.println("New call is running...");
-				System.out.println("new call is about to end...");
-				System.out.println("New call enede and notifiing holded call");
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		notify();
-		System.out.println("Holded call received...");
-		System.out.println("All call done...");
-
-	}
-
+        if (flag == 1) {
+            System.out.println(Thread.currentThread().getName() + " is putting its call on hold (waiting)...");
+            try {
+                wait(); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " resumed its call after being notified.");
+        } else {
+           
+            System.out.println(Thread.currentThread().getName() + " is notifying waiting call...");
+            notify(); 
+        }
+        System.out.println(Thread.currentThread().getName() + " call is completed.");
+    }
 }
 
 class Call1 extends Thread {
 
-	Calling c;
+    Calling c;
 
-	public Call1(Calling c) {
-		this.c = c;
-	}
+    public Call1(Calling c) {
+        this.c = c;
+    }
 
-	@Override
-	public void run() {
-		c.doCall(1);
-	}
-
+    @Override
+    public void run() {
+        c.doCall(1);
+    }
 }
 
 class Call2 extends Thread {
 
-	Calling c;
+    Calling c;
 
-	public Call2(Calling c) {
+    public Call2(Calling c) {
+        this.c = c;
+    }
 
-		this.c = c;
-	}
-
-	@Override
-	public void run() {
-		c.doCall(0);
-	}
-
+    @Override
+    public void run() {
+        c.doCall(0);
+    }
 }
 
 public class ThreadDemo10 {
 
-	public static void main(String[] args) {
-		
-		Calling calling = new Calling();
-		Call1 c1 = new Call1(calling);
-		Call2 c2 = new Call2(calling);
-		
-		c1.setName("AMIT");
-		c2.setName("RAJ");
-		
-		c1.start();
-		c2.start();
-		
-		
+    public static void main(String[] args) {
+        Calling calling = new Calling();
+        Call1 c1 = new Call1(calling);
+        Call2 c2 = new Call2(calling);
 
-	}
+        c1.setName("AMIT");
+        c2.setName("RAJ");
+
+        c1.start();
+        c2.start();
+    }
 }
